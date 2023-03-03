621416e88aed57809a91a4d2c57892feb4dd22c7
rjhoover <robert.hoover@mac.com>
2023-03-02 16:48:53 -0800

    initial commit

--- Challenge03/Extension-String.swift
+++ Challenge03/Extension-String.swift
@@ -5,34 +5,23 @@
 //  Created by Robert Hoover on 2023-02-24.
 //
 
+// MARK: - extensions
 extension String {
-    // https://en.wikipedia.org/wiki/Knuth–Morris–Pratt_algorithm
-    func indices(of string: String) -> [Int] {
-        guard !string.isEmpty else { return [] }
+    func indices(for letter: String) -> [Int] {
+        guard !letter.isEmpty else { return [] }
         
-//        let search = self.utf8.map { $0 }
-//        let word = string.utf8.map { $0 }
-        let search = self.map { $0 }
-        let word = string.map { $0 }
+        let stringArray = self.map { String($0) }  // convert string to array of string
+        var positionsArray = [Int]()
         
-        var indices = [Int]()
-        
-        var m = 0, i = 0
-        while m + i < search.count {
-            if word[i] == search[m + i] {
-                if i == word.count - 1 {
-                    indices.append(m)
-                    m += i + 1
-                    i = 0
-                } else {
-                    i += 1
-                }
+        for i in 0..<stringArray.count {
+            if letter == stringArray[i] {
+                positionsArray.append(i)
             } else {
-                m += 1
-                i = 0
+                continue
             }
         }
         
-        return indices
+        return positionsArray
     }
 }
+
--- Challenge03/ViewController.swift
+++ Challenge03/ViewController.swift
@@ -459,23 +459,3 @@ class ViewController: UIViewController {
     }
 
 }
-
-// MARK: - extensions
-extension String {
-    func indices(for letter: String) -> [Int] {
-        guard !letter.isEmpty else { return [] }
-        
-        let stringArray = self.map { String($0) }  // convert string to array of string
-        var positionsArray = [Int]()
-        
-        for i in 0..<stringArray.count {
-            if letter == stringArray[i] {
-                positionsArray.append(i)
-            } else {
-                continue
-            }
-        }
-        
-        return positionsArray
-    }
-}
